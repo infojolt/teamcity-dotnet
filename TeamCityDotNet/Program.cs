97e3179563dfd5044ed34582ac4ea3da80b1c8a4
Michael <michaelbailey@gmail.com>
2021-03-08 14:03:24 +0000

    Add project files.

---
+++ TeamCityDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamCityDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
